a2f8d2e72576da5b336f1023d49b10ca32d5b84b
Piyush jangir <piyushjangir915@gmail.com>
2025-09-16 19:08:52 +0530

    code.py

--- code.py
+++ code.py
@@ -1,26 +1,46 @@
-import random
+from random import randint
 
-def number_guessing_game():
+EASY_LEVEL_TURNS = 10
+HARD_LEVEL_TURNS = 5
+
+def check_answer(user_guess, actual_answer, turns):
+    if user_guess > actual_answer:
+        print("Too high.")
+        return turns - 1
+    elif user_guess < actual_answer:
+        print("Too low.")
+        return turns - 1
+    else:
+        print(f"You got it! The answer was {actual_answer}")
+
+
+def set_difficulty():
+    level = input("Choose a difficulty. Type 'easy' or 'hard': ")
+    if level == "easy":
+        return EASY_LEVEL_TURNS
+    else:
+        return HARD_LEVEL_TURNS
+
+
+def game():
     print("Welcome to the Number Guessing Game!")
-    print("I have selected a number between 1 and 100. Can you guess it?")
-    secret_number = random.randint(1, 100)
-    attempts = 0
+    print("I'm thinking of a number between 1 and 100.")
+    answer = randint(1, 100)
+    turns = set_difficulty()
 
-    while True:
-        try:
-            guess = int(input("Enter your guess: "))
-            attempts += 1
-            if guess < 1 or guess > 100:
-                print("Please enter a number between 1 and 100.")
-            elif guess < secret_number:
-                print("Too low! Try again.")
-            elif guess > secret_number:
-                print("Too high! Try again.")
-            else:
-                print(f"Congratulations! You guessed it in {attempts} attempts.")
-                break
-        except ValueError:
-            print("Invalid input! Please enter an integer.")
+    guess = 0
+    while guess != answer:
+        print(f"You have {turns} attempts remaining to guess the number.")
+        guess = int(input("Make a guess: "))
+        turns = check_answer(guess, answer, turns)
+        if turns == 0:
+            print("You've run out of guesses, you lose.")
+            return
+        elif guess != answer:
+            print("Guess again.")
 
-if __name__ == "__main__":
-    number_guessing_game()
+game()
+
+
+
+
